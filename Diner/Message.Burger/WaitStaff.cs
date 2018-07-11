3670e823c619dc62e569317f0ab5be7cd8c51052
condron <chris@condron.net>
2018-07-11 11:18:06 -0400

    define waitstaff

---
+++ Diner/Message.Burger/WaitStaff.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message.Burger
+{
+    public class WaitStaff:
+        IHandle<CustomerArrived>,
+        IHandle<FoodRequested>,
+        IHandle<OrderUp> {
+
+        private readonly IPublish _bus;
+
+        public WaitStaff(IPublish bus) {
+            _bus = bus;
+        }
+        public void Handle(CustomerArrived msg) {
+            //seat customer
+            _bus.Publish(new CustomerSeated());
+        }
+
+        public void Handle(FoodRequested msg) {
+            //take order
+            _bus.Publish(new OrderIn());
+        }
+
+        public void Handle(OrderUp msg) {
+            //deliver food
+            _bus.Publish(new OrderDelivered());
+        }
+    }
+
+    public class OrderDelivered : Message { }
+
+    public class OrderIn : Message { }
+
+    public class CustomerSeated : Message { }
+
+    public class OrderUp : Message { }
+
+    public class FoodRequested : Message { }
+
+    public class CustomerArrived : Message { }
+}
