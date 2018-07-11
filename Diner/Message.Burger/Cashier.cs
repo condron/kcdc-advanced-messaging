ff487214b3d7e2d30a3464fcf371b513a37fa72c
condron <chris@condron.net>
2018-07-11 10:53:36 -0400

    refactor to messages: define cashier

---
+++ Diner/Message.Burger/Cashier.cs
@@ -0,0 +1,29 @@
+﻿namespace Message.Burger {
+    public class Cashier:
+        IHandle<CompletedOrder>,
+        IHandle<PaymentTendered>{
+        private readonly IPublish _bus;
+
+        public Cashier(IPublish bus) {
+            _bus = bus;
+        }
+
+        public void Handle(CompletedOrder msg) {
+            //todo: total order
+            _bus.Publish(new OrderTotaled());
+        }
+
+        public void Handle(PaymentTendered msg) {
+            //todo: process payment
+            _bus.Publish(new OrderPaid());
+        }
+    }
+
+    public class OrderPaid : Message { }
+
+    public class OrderTotaled : Message { }
+
+    public class PaymentTendered:Message { }
+
+    public class CompletedOrder:Message { }
+}
---
+++ Diner/Message.Burger/IHandle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message.Burger {
+    public interface IHandle<T> where T:Message
+    {
+        void Handle(T msg);
+    }
+    public abstract class Message{}
+}
